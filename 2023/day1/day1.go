bcf2c9cd7d88b81ef35d9ba6c6ef67f780cf154b
Chris Spalding <cspalding93@gmail.com>
2023-12-02 08:26:06 -0800

    day 1 part 1

--- 2023/day1/day1.go
+++ 2023/day1/day1.go
@@ -1,5 +1,11 @@
 package day1
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func GetFirstAndLastDigit(line string) int {
 	firstDigit := 0
 	lastDigit := 0
@@ -12,13 +18,26 @@ func GetFirstAndLastDigit(line string) int {
 			lastDigit = res
 		}
 	}
+
+	if lastDigit == 0 {
+		lastDigit = firstDigit
+	}
+
 	return (10 * firstDigit) + lastDigit
 }
 
-func Day1Part1(lines []string) int {
+func Day1Part1(filepath string) int {
+	file, err := os.Open(filepath)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return -1
+	}
+
+	scanner := bufio.NewScanner(file)
 	sum := 0
-	for _, line := range lines {
-		sum += GetFirstAndLastDigit(line)
+	for scanner.Scan() {
+		sum += GetFirstAndLastDigit(scanner.Text())
 	}
 	return sum
 }
--- 2023/main.go
+++ 2023/main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/cspalding/aoc/2023/day1"
 )
 
 func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Wrong number of args.\nUsage:\n\tgo run main.go DAY")
+		return
+	}
+
 	day := os.Args[1]
+	filename := os.Args[2]
 
 	switch day {
 	case "1":
-		Day1Part1()
+		res := day1.Day1Part1(filename)
+		fmt.Printf("Day 1 Part 1 Solution: %d\n", res)
 	default:
 		fmt.Printf("Solution not implemented for day %s", day)
 	}
